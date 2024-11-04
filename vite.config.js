67bd3d5c23f9c6044fbc7ee7cfef4a005a331f47
Neha singh <neha786.india@gmail.com>
2024-11-04 19:44:38 +0530

    deploy error

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/weather-app/',
+  base: '/AI_Day_Planner/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
